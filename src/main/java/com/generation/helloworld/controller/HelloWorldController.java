9c3a50f43c2f55b3c1c44df1f93b664d12749ecd
Vitoria Venturini <mvventurini9@gmail.com>
2022-06-30 09:31:38 -0300

    hello_world

--- src/main/java/com/generation/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/generation/helloworld/controller/HelloWorldController.java
@@ -12,4 +12,21 @@ public class HelloWorldController {
 	public String helloworld(){
 		return "Hello World!!!";
 	}
+	@GetMapping("/bsm")
+	public String bsm(){
+		return "Trabalho em equipe\n"
+				+ "Atenção aos Detalhes\n"
+				+ "Proatividaden"
+				+ "Comunicaçãon"
+				+ "Orientação ao Futuron"
+				+ "Responsabilidade Pessoal\n"
+				+ "Persistência\n"
+				+ "Mentalidade de Crescimento";
+	}
+	@GetMapping("/objetivos")
+	public String objetivos(){
+		return " Realizar todas as atividades da semana\n"
+				+ "Me comunicar mais com meus colegas de projeto \n"
+				+ "Ter mais proatividade\n";
+	}
 }
